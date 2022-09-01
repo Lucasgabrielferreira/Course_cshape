c14772b2e99fcd7e855df6efc0c731e18e6f7a7b
LUCAS GABRIEL FERREIRA <105209565+Lucasgabrielferreira@users.noreply.github.com>
2022-09-01 10:38:13 -0300

    Introdução à Programação Orientada a Objetos

---
+++ Exercicio 04/Exercicio 04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            ret.largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("AREA = " + ret.area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = " + ret.perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = " + ret.diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
